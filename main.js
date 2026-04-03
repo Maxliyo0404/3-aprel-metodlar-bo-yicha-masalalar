d25fc82baeb0cddf65ea920772f506bc682d9063
Mahliyo <gasandigital13@gmail.com>
2026-04-03 22:32:48 +0500

    for 2-masalasi lenght

--- main.js
+++ main.js
@@ -39,7 +39,7 @@
     
 // }
 // 2-masala
-let names = ['Farxod', 'Baxrom', 'Sherzod','Shokir'];
+// let names = ['Farxod', 'Baxrom', 'Sherzod','Shokir'];
 // for (let i = 0; i < names.length; i++){
 //     console.log(names[i]);
     
