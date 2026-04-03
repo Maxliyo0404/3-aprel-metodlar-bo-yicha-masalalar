2fb8ec2ec48747775470a18e06a46981b56eb754
Mahliyo <gasandigital13@gmail.com>
2026-04-03 22:34:30 +0500

    for 3-masalasi lenght

--- main.js
+++ main.js
@@ -15,32 +15,32 @@
 // let book = books.map((good) => {
 //     return{ name: good.name, year: good.year, reating: good.reating +1};
 // })
-// console.log(book);
+// // console.log(book);
 
-// 2.  ForEach metodi
+// // 2.  ForEach metodi
 
-// const numbers = [2, 3, 4, 5,];
-// const newNumber = [];
-// numbers.forEach(num => {
-//     newNumber.push(num ** 2)
-// });
-// console.log(newNumber);
-// 2-masala
-// let ismlar = ['Rano', 'Barno','Malika', 'Dilshoda', 'Gulirano'];
-// ismlar.forEach(function (ism){
-//     let yangi = ism.charAt().toUpperCase() + ism.slice(1).toLowerCase() + "xon";
-//     console.log(yangi);
+// // const numbers = [2, 3, 4, 5,];
+// // const newNumber = [];
+// // numbers.forEach(num => {
+// //     newNumber.push(num ** 2)
+// // });
+// // console.log(newNumber);
+// // 2-masala
+// // let ismlar = ['Rano', 'Barno','Malika', 'Dilshoda', 'Gulirano'];
+// // ismlar.forEach(function (ism){
+// //     let yangi = ism.charAt().toUpperCase() + ism.slice(1).toLowerCase() + "xon";
+// //     console.log(yangi);
     
-// })
-// For metodi
-let numbers = [2, 3, 4, 5, 6, 7];
-for (let i = 0; i < numbers.length; i ++) {
-    console.log(numbers[i]);
+// // })
+// // For metodi
+// let numbers = [2, 3, 4, 5, 6, 7];
+// for (let i = 0; i < numbers.length; i ++) {
+//     console.log(numbers[i]);
+    
+// // }
+// // 2-masala
+// // let names = ['Farxod', 'Baxrom', 'Sherzod','Shokir'];
+// // for (let i = 0; i < names.length; i++){
+// //     console.log(names[i]);
     
 // }
-// 2-masala
-// let names = ['Farxod', 'Baxrom', 'Sherzod','Shokir'];
-// for (let i = 0; i < names.length; i++){
-//     console.log(names[i]);
-    
-}
