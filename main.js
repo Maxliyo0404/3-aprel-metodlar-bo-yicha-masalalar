466d9fb5f2f63fc17234ea3c0c7de95cfdc28ba3
Mahliyo <gasandigital13@gmail.com>
2026-04-03 15:46:37 +0500

    map metodini 2-masalani boshladim name year qo'shib ortirish

--- main.js
+++ main.js
@@ -7,7 +7,10 @@
 // 2-masala
 let books = [
     { name: 'Ikki eshik orasi', year: 2005, reating : 9 },
-    { name: 'Otkan kunlar', year: 2008, reating : 8 },
-    { name: 'Ikki eshik orasi', year: 2012, reating : 8 },
-    { name: 'Ikki eshik orasi', year: 2012, reating : 8 },
+    { name: 'Otkan kunlar', year: 2008, reating : 7 },
+    { name: 'Sarob', year: 2010, reating : 5 },
+    { name: 'Sotilayotgan kasaliklar', year: 2025, reating : 3 },
 ]
+let book = books.map((good) => {
+    return
+})
