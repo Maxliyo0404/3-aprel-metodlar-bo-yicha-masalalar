096222c3cba665a28245dc433dae470719211e24
Mahliyo <gasandigital13@gmail.com>
2026-04-03 15:37:58 +0500

    map metodini 2-masalani boshladim

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Map metodi
-let numbers =[2, 3, 4, 5, 6, 7, 8, 9];
-let newNumber = numbers.map((num) => {
-    return num ** 2;
-})
-console.log(newNumber);
+// let numbers =[2, 3, 4, 5, 6, 7, 8, 9];
+// let newNumber = numbers.map((num) => {
+//     return num ** 2;
+// })
+// console.log(newNumber);
+// 2-masala
+let fruits = [
+    { name: 'Apple red', }
+]
