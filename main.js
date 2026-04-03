9f585d4958d802357eb6bb15253fc5cb8da86b3b
Mahliyo <gasandigital13@gmail.com>
2026-04-03 15:51:07 +0500

    ForEach metodini boshladim

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 // Map metodi
+
 // let numbers =[2, 3, 4, 5, 6, 7, 8, 9];
 // let newNumber = numbers.map((num) => {
 //     return num ** 2;
 // })
 // console.log(newNumber);
 // 2-masala
-let books = [
-    { name: 'Ikki eshik orasi', year: 2005, reating : 9 },
-    { name: 'Otkan kunlar', year: 2008, reating : 7 },
-    { name: 'Sarob', year: 2010, reating : 5 },
-    { name: 'Sotilayotgan kasaliklar', year: 2025, reating : 3 },
-]
-let book = books.map((good) => {
-    return{ name: good.name, year: good.year, reating: good.reating +1};
-})
-console.log(book);
+// let books = [
+//     { name: 'Ikki eshik orasi', year: 2005, reating : 9 },
+//     { name: 'Otkan kunlar', year: 2008, reating : 7 },
+//     { name: 'Sarob', year: 2010, reating : 5 },
+//     { name: 'Sotilayotgan kasaliklar', year: 2025, reating : 3 },
+// ]
+// let book = books.map((good) => {
+//     return{ name: good.name, year: good.year, reating: good.reating +1};
+// })
+// console.log(book);
+
+//  ForEach metodi
