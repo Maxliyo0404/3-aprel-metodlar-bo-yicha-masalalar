201e35aef9bf6587905e7a93ebd54844318caa04
Mahliyo <gasandigital13@gmail.com>
2026-04-04 21:04:55 +0500

    Filter metodi 2-masala boshladim fruits

--- main.js
+++ main.js
@@ -65,5 +65,5 @@
  
 // 2-masala
 const fruits = ['banana', 'apple', 'oringe','pear', 'peach'];
-let newFruit = fruits.filter(fruit => fruit.length, 5);
+let newFruit = fruits.filter(fruit => fruit.length > 4);
 console.log(newFruit);
